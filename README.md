6ff18484e7751be6ec37b1e0582551d9e5df0754
Chris Minnick <chris@minnick.com>
2022-10-19 14:49:10 -0400

    challenge 1

--- README.md
+++ README.md
@@ -9,3 +9,15 @@ Demo react app with animated timer
 ## to run
 
 `npm run dev`
+
+## Your job
+
+### Challenge #1
+
+1. Get contestant data into state
+2. Get currentTime prop into Race component
+3. Use currentTime somehow to change xpos
+
+### Challenge #2
+
+### Challenge #3
--- src/components/Race.jsx
+++ src/components/Race.jsx
@@ -1,8 +1,17 @@
+import { useState, useEffect } from 'react';
 import Contestant from './Contestant.jsx';
 import { contestantStyles } from '../styles';
-import { contestants } from '../assets/data';
+import { contestantData } from '../assets/data';
 
-function Race() {
+function Race(props) {
+  const [contestants, setContestants] = useState(contestantData);
+  useEffect(() => {
+    let newContestantPos = contestants.map((contestant) => {
+      return { ...contestant, xpos: contestant.xpos + 10 };
+    });
+    setContestants(newContestantPos);
+  }, [props.currentTime]);
+
   return (
     <div style={contestantStyles.contestantList}>
       {contestants.map((contestant) => (
