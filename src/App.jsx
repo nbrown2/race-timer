3b55b63464eb0f25e21fa750cd930115b0737e3f
Chris Minnick <chris@minnick.com>
2022-10-19 16:13:13 -0400

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Configuration from './components/Configuration';
 import Timer from './components/Timer';
@@ -20,8 +20,10 @@ function App() {
   return (
     <>
       <Router>
+        <Link to="/config">Config Screen</Link> |{' '}
+        <Link to="/game">Game Screen</Link>
         <Routes>
-          <Route path="/" element={<Configuration />} />
+          <Route path="/config" element={<Configuration />} />
           <Route
             path="/game"
             element={
