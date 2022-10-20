9dbe11d2fc49e5f8610f27160bc1b535c52b1b67
Jean-Luc LaCosse <97177770+jeanlacosse@users.noreply.github.com>
2022-10-20 14:21:44 -0400

    comments added to race page

--- src/components/Race.jsx
+++ src/components/Race.jsx
@@ -2,21 +2,21 @@ import { useState, useEffect } from "react";
 import Contestant from "./Contestant.jsx";
 import { contestantStyles } from "../styles";
 import { contestantData } from "../assets/data";
-import { useNavigate } from "react-router-dom";
 
 function Race(props) {
   const [contestants, setContestants] = useState(contestantData);
-  // let navigateTo = useNavigate();
+
 
   const handleStop = () => {
     props.stopTimer();
-    // navigateTo('/config');
   };
 
+  // This will stop the timer from going below 0 seconds
   if (props.currentTime < 1) {
     handleStop();
   }
 
+  // this function is called on reset button to set the contestants back to 0 xpos
   const reset = () => {
     let newContestantPos = contestants.map((contestant) => {
       return {
@@ -47,6 +47,8 @@ function Race(props) {
         />
       ))}
       <button onClick={handleStop}>Stop the Timer!</button>
+
+      {/* this button triggers the reset function */}
       <button onClick={reset}>Reset!</button>
     </div>
   );
